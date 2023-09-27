4a7629371de1f3e80a741caff47d4b76e29dde78
Prabakaran M S <ms.prabakaran11@gmail.com>
2023-09-28 00:54:28 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let is24Hours = false;
+
+function toggleFormat() {
+    is24Hours = ~is24Hours;
+}
+
+function showTime() {
+    const now = new Date();
+    let hour = now.getHours();
+    let min = now.getMinutes();
+    let sec = now.getSeconds();
+    const clock = document.getElementById('clock');
+
+    if (is24Hours) {
+        clock.textContent = `${hour}:${min<10?'0':''}${min}:${sec<10?'0':''}${sec}`;
+    } else {
+        if (hour > 12) {
+            clock.textContent = `${hour -12}:${min<10?'0':''}${min}:${sec<10?'0':''}${sec} PM`;
+        } else {
+            clock.textContent = `${hour +12}:${min<10?'0':''}${min}:${sec<10?'0':''}${sec} AM`;
+        }
+    }
+}
+
+setInterval(showTime, 1000);
